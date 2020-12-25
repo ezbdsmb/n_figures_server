13e573aadf3b9b11482181c7e078a4a02231512f
bnovikov <nvikv-brja@yandex.ru>
2020-12-25 16:57:19 +0300

    refactor: rename test client and move to test folder

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,8 +1,8 @@
 import socket
 import sys
 
-HOST, PORT = "localhost", 9999
-data = "init 1"
+HOST, PORT = "localhost", 9998
+data = "init Q"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
